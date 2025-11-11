4bb850e281f0370341cfe6313209f16c3dfe8544
Saieed Rahman <133708725+Saiedeu@users.noreply.github.com>
2025-11-11 12:37:03 +0300

    Add files via upload

---
+++ install/eb_verification.php
@@ -0,0 +1,15 @@
+<?php
+// EB License System Verification File v2.0.0
+// DO NOT MODIFY THIS FILE
+return array (
+  'license_key' => 'EB-NACHO-VARGA-BETTE-RCALL',
+  'domain' => 'demo.exchange-bridge.rf.gd',
+  'status' => 'active',
+  'hash' => 'd9257223bab0005dd60b9e0a4312da5f',
+  'last_check' => 1762852951,
+  'validation_type' => 'automatic',
+  'expires' => 0,
+  'server_status' => 'active',
+  'created_at' => 1762852951,
+  'version' => '2.0.0',
+);
